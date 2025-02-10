20798a74521eb6eac3c8455b3ecd447b89f28b24
louvardion <diegoamaral321@gmail.com>
2025-02-10 19:54:13 -0300

    5° Commit - Correção do código de gradiente para que fique funcional

--- Gerador de Gradiente PPM/gerador_gradiente.c
+++ Gerador de Gradiente PPM/gerador_gradiente.c
@@ -8,7 +8,7 @@ int main(){
     int largura = 256, altura = 256;
     
     //Ponteiro que criará um file para ser escrito.
-    FILE *gradiente = fopen("gradiente.ppm");
+    FILE *gradiente = fopen("gradiente.ppm", "w");
 
     //Escrita dentro do file.
     fprintf(gradiente,"P3\n%d %d\n255\n", largura, altura);
@@ -16,10 +16,13 @@ int main(){
         //Loop for para preencher os pixeis:
         for(int y = 0; y < altura; y++){
             for(int x = 0; x < largura; x++){
-                int r = (1 - (x / largura)) * 255;   //Gradiente do Vermelho para o azul na horizontal.
+                int r = (1 - ((float)x / largura)) * 255;   //Gradiente do Vermelho para o azul na horizontal.
                 int g = 0;
-                int b = (x / largura) * 255; 
+                int b = ((float)x / largura) * 255;
+                
+                fprintf(gradiente, "%d %d %d ", r, g, b); //Escrevendo os pixeis no file
             }
+            fprintf(gradiente, "\n"); //começando nova linha de pixeis a cada iteração do loop
         }
     
         fclose(gradiente); //Fechando o file escrito.
