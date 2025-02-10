4233e3b7c444868ba6404897f48ea5a67e2bf8d5
louvardion <diegoamaral321@gmail.com>
2025-02-10 01:50:39 -0300

    1º commit: código base que gera imagem em ppm

---
+++ gerador.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+
+/* Projeto de teste para aprendizado
+Por:    Diego Amaral
+        Estudante de Análise e Desenvolvimento de Sistemas  
+Passos:
+    1 - Criar um programa em C que crie um arquivo ".ppm".
+    2 - Preencher esse arquivo com píxels coloridos como um gradiente, ou formas simples. 
+    3 - Abrir imagem em um visualizador compatível.
+
+ Sobre formato .ppm:
+    Um arquivo .ppm começa com:
+
+    P3
+    LARGURA ALTURA
+    255
+    R G B R G B R G B...
+
+    Cada trio de "R G B" define a cor de um píxel. O valor varia de 0 a 255.
+*/
+
+int main () {
+    
+    //Declaro as variáveis de altura e largura da imagem
+    int largura = 256, altura = 256;
+
+    FILE *imagem = fopen("imagem.ppm", "w");
+
+    fprintf(imagem, "P3\n%d %d\n255\n", largura, altura);
+
+        for (int y = 0; y < altura; y++){
+            for (int x = 0; x < largura; x++){
+                int r = x;    //Vermelho variando na horizontal
+                int g = y;    //Verde variando na vertical
+                int b = 128;  //O Azul fica fixo
+                fprintf(imagem, "%d %d %d", r, g, b);
+            }
+            fprintf(imagem, "\n");
+        }
+
+        fclose(imagem);
+        printf("Imagem gerada: imagem.ppm\n");
+    return 0;
+}
